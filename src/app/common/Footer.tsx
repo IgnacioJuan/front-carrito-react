ba9707dd1137f6726b8997d7b173276a76f83ecd
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-03-31 23:09:32 -0500

    footer de chat gpt

--- src/app/common/Footer.tsx
+++ src/app/common/Footer.tsx
@@ -1,44 +1,37 @@
 import React from 'react';
-import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 import "../styles/Footer.css";
-export const Footer = () => {
-  return (
-    <div className="footer" data-background-color="gray">
-      <div className="container" data-gray-color="gray">
-        <a className="foot-brand" href="#creative">Cities & Maiki</a>
-          <ul className="pull-center">
-            <li>
-              <a href="#" className="nav-link">Proyectos</a>
-            </li>
-            <li>
-              <a href="#" className="nav-link">Paginadores</a>
-            </li>
-            <li>
-              <a href="#" className="nav-link">Informacion</a>
-            </li>
-            <li>
-              <a href="#" className="nav-link">Sobre Nosotros</a>
-            </li>
-          </ul>
 
-          <ul className="social-buttons pull-right">
-            <li>
-              <a href="#" className="btn btn-icon btn-neutral btn-twitter">
-                <i className="fa fa-twitter"/>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="btn btn-icon btn-neutral btn-dribbble">
-                <i className="fa fa-dribbble"/>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="btn btn-icon btn-neutral btn-google">
-                <i className="fa fa-google-plus"/>
-              </a>
-            </li>
-          </ul>
+export const Footer = () => {
+  
+  
+  return (
+    <footer className="footer-container">
+      <div className="social-icons-container">
+        <a href="https://github.com/">
+          <img
+            src="https://img.icons8.com/ios/50/000000/github--v1.png"
+            alt="Github"
+            className="social-icon"
+          />
+        </a>
+        <a href="https://www.linkedin.com/">
+          <img
+            src="https://img.icons8.com/ios/50/000000/linkedin.png"
+            alt="LinkedIn"
+            className="social-icon"
+          />
+        </a>
+        <a href="https://twitter.com/">
+          <img
+            src="https://img.icons8.com/ios/50/000000/twitter--v1.png"
+            alt="Twitter"
+            className="social-icon"
+          />
+        </a>
       </div>
-    </div>
+      <div className="text-container">
+        <span className="text">&copy; 2023 Tu Empresa</span>
+      </div>
+    </footer>
   );
 }
