ac503cf1da20bd1dbaa1c7181167d56aea25b992
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2023-04-07 00:36:00 -0500

    Update Cart.tsx

--- src/app/views/Catalogue/Cart.tsx
+++ src/app/views/Catalogue/Cart.tsx
@@ -1,40 +1,46 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { removeProductFromCart } from '../../reducers/cart/cartSlice';
 import { IProduct } from '../../interfaces/IProduct';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import { Button } from 'primereact/button';
 
 export const Cart = () => {
   const dispatch = useDispatch();
   const { productsList } = useSelector((state: any) => state?.cart);
 
-  const handleRemoveProduct = (productId:any) => dispatch(removeProductFromCart(productId));
+  const handleRemoveProduct = (productId: any) => dispatch(removeProductFromCart(productId));
+  const boton = () => {
+    return {
 
+    }
+  }
   return (
     <>
       <h2>Cart</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Category</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {productsList.map((product: IProduct) => {
-            return (
-              <tr key={product.id_producto}>
-                <th scope="row">{product.id_producto}</th>
-                <td scope="row">{product.nom_Producto}</td>
-                <td scope="row">{product.valor_unitario}</td>
-                <td scope="row">{product.categoria.id_categoria}</td>
-                <td scope="row"><button className="btn btn-danger" onClick={() => handleRemoveProduct(product.id_producto)}>Delete</button></td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      
+      <DataTable
+        value={productsList}
+        responsiveLayout="scroll"
+        style={{ textAlign: "center" }}
+        selectionMode="single"
+        paginator
+        rows={5}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+      >
+        <Column field="id_producto" header="ID"></Column>
+        <Column field="nom_Producto" header="NAME"></Column>
+        <Column field="stock" header="STOCK"></Column>
+        <Column field="descripcion" header="DESCRIPTION"></Column>
+        <Column field="valor_unitario" header="UNIT VALUE"></Column>
+        {/* <Column field="foto" header="IMAGE" body={imageBodyTemplate} /> */}
+        <Column field="categoria.nombre_categoria" header="CATEGORY"></Column>
+        <Column header="ACTIONS" body={(rowData) => (
+          <Button  onClick={() => handleRemoveProduct(rowData.id_producto)}>Delete</Button>
+        )}
+        />
+
+      </DataTable>
     </>
   )
 }
